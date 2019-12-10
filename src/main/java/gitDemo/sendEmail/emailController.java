788d968f24af32d96a7f7b8c8875bc86d83d1632
lzh5435 <lzh5435@163.com>
2019-12-10 16:35:31 +0800

    1

--- src/main/java/gitDemo/sendEmail/emailController.java
+++ src/main/java/gitDemo/sendEmail/emailController.java
@@ -1,22 +1,40 @@
 package gitDemo.sendEmail;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import java.util.Arrays;
+import java.util.Map;
 
-@Controller
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+
+@RestController
 public class emailController {
 
 	@Autowired
 	SendEmail sendEmail;
 	
 	@RequestMapping("send")
-	@ResponseBody
 	public void sendContent(String to,String subject,String content) {
 		to = "825805836@qq.com";
 		subject = "表白开始";
 		content="快来看";
 		sendEmail.sendSimpleMail(to, subject, content);
+	}
+	
+	@RequestMapping(value="findpost",method = RequestMethod.POST)
+	public void test(@RequestBody String data) {
+		System.err.println(JSONObject.parse(data));
+		System.out.println(data);
 	}
 }
